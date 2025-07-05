df4f50f5f6a91201da4e4166b1f59997087a7bfd
AnastasiyaYurc <yurcanastasiya@gmail.com>
2025-07-05 11:00:10 +0300

    fix: added content for banner

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -5,17 +5,18 @@ const swiperBanner = new Swiper('.swiper-banner', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  spaceBetween: 10,
+  spaceBetween: 24,
   // If we need pagination
+
+    navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+    },
+
   pagination: {
       clickable: true,
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
 
 });
