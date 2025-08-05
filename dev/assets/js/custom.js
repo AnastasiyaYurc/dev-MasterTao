d0e4166a080977c3f844ffb34c3d1188ba16bb7a
AnastasiyaYurc <yurcanastasiya@gmail.com>
2025-08-06 01:17:09 +0300

    feat: added bg-accent overlay

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -8,12 +8,9 @@ const btnBurger = document.querySelector('.hamburger')
 btnBurger.addEventListener('click', ()=> btnBurger.classList.toggle('is-active'))
 
 const swiperBanner = new Swiper('.swiper-banner', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
   spaceBetween: 24,
-  // If we need pagination
-
     navigation: {
          nextEl: '.swiper-button-next',
          prevEl: '.swiper-button-prev',
@@ -48,18 +45,29 @@ fileInput.forEach(item => {
 })
 
 
+const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length
 
-
-
-
-
-
-//const fileInput = document.querySelector('#calculation-file');
-//const fileName = document.querySelector('#file-name')
-
-//fileInput.addEventListener('change', () => {
-  // fileName.innerHTML = fileInput.files[0].name;
-//});
-
-
-
+const swiperAdvantages = new Swiper('#advantages', {
+    spaceBetween: 12,
+    loop: true,
+    simulateTouch: true,
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        enabled: true,
+    },
+    breakpoints: {
+        1025: {
+            spaceBetween: 0,
+            slidesPerView: advantagesSlideLength,
+            slidesPerGroup: advantagesSlideLength,
+            loop: false,
+            simulateTouch: false,
+            pagination: {
+                enabled: false,
+                el: '.swiper-pagination',
+            }
+        }
+    }
+});
