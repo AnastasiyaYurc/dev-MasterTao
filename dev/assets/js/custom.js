50ac43a504b1cf338aa08c447f28241c1811baf6
AnastasiyaYurc <yurcanastasiya@gmail.com>
2025-08-16 11:46:22 +0300

    feat: service, delivery & about

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -2,21 +2,21 @@ const selects = document.querySelectorAll('select')
 selects.forEach(item => NiceSelect.bind(item))
 
 const btnBurger = document.querySelector('.hamburger')
-btnBurger.addEventListener('click', ()=> btnBurger.classList.toggle('is-active'))
+btnBurger.addEventListener('click', () => btnBurger.classList.toggle('is-active'))
 
 const swiperBanner = new Swiper('.swiper-banner', {
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 24,
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 24,
     navigation: {
-         nextEl: '.swiper-button-next',
-         prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
-  pagination: {
-      clickable: true,
-    el: '.swiper-pagination',
-  },
+    pagination: {
+        clickable: true,
+        el: '.swiper-pagination',
+    },
 });
 
 const header = document.querySelector('header')
@@ -28,7 +28,7 @@ banner.setAttribute('style', '--header-height: ' + headerHeight + 'px')
 const inputCheckbox = document.querySelectorAll('input[type="checkbox"]');
 
 inputCheckbox.forEach(item => {
-      item.closest('.input-box').classList.add('input-box_checkbox')
+    item.closest('.input-box').classList.add('input-box_checkbox')
 })
 
 const fileInput = document.querySelectorAll('[type="file"]');
@@ -48,6 +48,7 @@ const swiperAdvantages = new Swiper('#advantages', {
     simulateTouch: true,
     slidesPerView: 1,
     roundLengths: true,
+
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -66,4 +67,43 @@ const swiperAdvantages = new Swiper('#advantages', {
             }
         }
     }
+});
+
+
+document.querySelectorAll('.template-carousel').forEach((carouselEl, index) => {
+    const swiper = new Swiper(carouselEl.querySelector('.swiper'), {
+        slidesPerView: 1,
+        direction: 'horizontal',
+        loop: true,
+        spaceBetween: 30,
+
+        navigation: {
+            nextEl: carouselEl.querySelector('.swiper-button-next'),
+            prevEl: carouselEl.querySelector('.swiper-button-prev'),
+        },
+
+        pagination: {
+            el: carouselEl.querySelector('.swiper-pagination'),
+            clickable: true,
+        },
+
+        breakpoints: {
+            830: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+                pagination: {
+                    enabled: false,
+                    el: carouselEl.querySelector('.swiper-pagination'),
+                }
+            },
+            1400: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                pagination: {
+                    enabled: false,
+                    el: carouselEl.querySelector('.swiper-pagination'),
+                }
+            }
+        }
+    });
 });
